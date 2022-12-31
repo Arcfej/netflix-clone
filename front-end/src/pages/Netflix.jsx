3b2a57382fbb3e95b538cabd5b26858a5af0a936
Arcfej <mayer.miklos91@gmail.com>
2022-12-31 22:05:28 +0000

    Get genres and movies from TMDB with redux

--- front-end/src/pages/Netflix.jsx
+++ front-end/src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Navbar from "../components/navbar";
 import { FaPlay, } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
@@ -7,16 +7,29 @@ import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
 
 const Netflix = () => {
     const [isScrolled, setIsScrolled] = useState(false);
     const navigate = useNavigate();
 
+    const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+    const movies = useSelector((state) => state.netflix.movies);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getGenres());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (genresLoaded) dispatch(fetchMovies({ type: "all" }));
+    });
+
     window.onscroll = () => {
         setIsScrolled(window.scrollY === 0 ? false : true);
         return () => window.onscroll = null;
     };
-
 
     return (
         <Container>
