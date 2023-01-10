516bd97ea33e562a4fbdd8a6fbd0da54d4920884
Arcfej <mayer.miklos91@gmail.com>
2023-01-10 20:06:10 +0000

    Test CI/CD 1

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+// Test deployment
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
