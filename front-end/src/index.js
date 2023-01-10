8540769d8e38430e0cacffad127680cf78e9e190
Arcfej <mayer.miklos91@gmail.com>
2023-01-10 21:20:23 +0000

    Abandon CI/CD tests

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,7 +5,6 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-// Test deployment 8
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
